432a9ff5c3866e94a8ba4cb801fc3932e32a96b3
Mak <makdiwan@yahoo.com>
2024-12-29 20:23:32 -0500

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by MAKARAND DIWAN on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by MAKARAND DIWAN on 12/29/24.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
